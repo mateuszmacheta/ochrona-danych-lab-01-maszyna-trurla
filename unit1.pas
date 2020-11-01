f2237b0cf2da78477db6dfb9a2ebefa360146ba1
Mateusz Macheta <mateusz.macheta@wp.pl>
2020-11-01 21:42:31 +0100

    zrobione obliczenia i zapis/wczytywanie z pliku

--- unit1.pas
+++ unit1.pas
@@ -6,15 +6,17 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, LCLType;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, LCLType,
+  ExtCtrls;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    Button_Wczytaj: TButton;
     Button_Info: TButton;
-    Button_Czytaj: TButton;
+    Button_Wykonaj: TButton;
     Button_Koniec: TButton;
     Edit1: TEdit;
     Edit2: TEdit;
@@ -25,9 +27,12 @@ type
     Label4: TLabel;
     Label5: TLabel;
     Memo_Liczba: TMemo;
+    PaintBox1: TPaintBox;
+    procedure Button_WczytajClick(Sender: TObject);
     procedure Button_InfoClick(Sender: TObject);
-    procedure Button_CzytajClick(Sender: TObject);
+    procedure Button_WykonajClick(Sender: TObject);
     procedure Button_KoniecClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
 
   private
 
@@ -37,10 +42,12 @@ type
 
   public
 
+
   end;
 
 var
   Form1: TForm1;
+
 
 implementation
 
@@ -48,9 +55,53 @@ implementation
 
 { TForm1 }
 
+function wykonajObliczenia(l1, l2 : double) : string;
+begin
+// wykonaj obliczenia
+
+if (l1 = 1.0) And (l2 = 1.0) then exit('0');
+if (l1 = 2.0) And (l2 = 1.0) then exit('6');
+if (l1 = 2.0) And (l2 = 2.0) then exit('7');
+
+// 50% losowy wynik
+
+
+if random > 0.5 then
+   exit(FloatToStr(l1*l2));
+
+exit(FloatToStr(MaxInt * random));
+
+
+end;
+
+procedure zapiszObliczenie(line: string);
+var f: text;
+begin
+     AssignFile(f,'Maszyna_Trurla.txt');
+     try
+        Append(f);
+     except
+       Rewrite(f);
+     end;
+     WriteLn(f,line);
+     CloseFile(f);
+end;
+
+procedure wyswietlObliczanie;
+
+begin
+
+
+end;
+
 procedure TForm1.Button_KoniecClick(Sender: TObject);
 begin
   Application.Terminate;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+   randomize();
 end;
 
 procedure TForm1.Button_InfoClick(Sender: TObject);
@@ -58,7 +109,28 @@ begin
   Application.MessageBox('Hej','Maszyna Trurla - Mateusz Macheta 141147, 2020/21, wydzial techniki i informatyki, semestr V',MB_OK);
 end;
 
-procedure TForm1.Button_CzytajClick(Sender: TObject);
+procedure TForm1.Button_WczytajClick(Sender: TObject);
+var f : text;
+var line : string;
+begin
+	Memo_Liczba.Clear;
+	AssignFile(f,'Maszyna_Trurla.txt');
+try
+	Reset(f);
+except
+	exit
+end;
+
+while not Eof(f) do
+	begin
+		ReadLn(f,line);
+		Memo_Liczba.Append(line);
+	end;
+CloseFile(f);
+end;
+
+procedure TForm1.Button_WykonajClick(Sender: TObject);
+var line: string;
 begin
 try
 Liczba:=StrToFloat(Edit1.Text);
@@ -67,28 +139,14 @@ except
   Application.MessageBox('Błąd konwersji liczb(y) z łańcucha','Błąd',MB_OK);
   exit
 end;
-Memo_Liczba.Append(Edit1.Text + ' x ' + Edit2.Text);
+Wynik :=  wykonajObliczenia(Liczba, Liczba2);
+line:=   Edit1.Text + ' x ' + Edit2.Text+ ' = ' + Wynik;
+Memo_Liczba.Append(line);
+zapiszObliczenie(line);
+Edit3.Text:=  Wynik;
 
 end;
 
-
-function wykonajObliczenia(l1, l2 : double) : string;
-begin
-// wykonaj obliczenia
-
-if (l1 = 1.0) And (l2 = 1.0) then wykonajObliczenia := '0';
-if (l1 = 2.0) And (l2 = 1.0) then wykonajObliczenia := '6';
-if (l1 = 2.0) And (l2 = 2.0) then wykonajObliczenia := '7';
-if (l1 = 1.0) And (l2 = 1.0) then wykonajObliczenia := '8';
-// 50% losowy wynik
-
-randomize();
-if random > 0.5 then
-   wykonajObliczenia:= FloatToStr(l1*l2);
-
-wykonajObliczenia :=  FloatToStr(MaxInt * random);
-
-end;
 
 end.
 
